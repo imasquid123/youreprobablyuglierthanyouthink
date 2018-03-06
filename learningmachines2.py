ea38a567b339b5b8d34413c2f9c9dad1595e5c20
maya <maya.schmidt@baxter-academy.org>
2018-03-06 09:48:27 -0500

    using tensorflow and giving up on pandas

---
+++ learningmachines2.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import csv
+TRAIN_URL = "PRECIP_HLY_sample_csv.csv"
+
+def __init__(self):
+	datamover = PyDict_SetItem()
+	dict[dataholder] = []
+
+def input_training_data(TRAIN_URL):
+	tf.data.Dataset.from_tensors(TRAIN_URL)
+
+def put_data(datamover, TRAIN_URL):
+
+	with open("PRECIP_HLY_sample_csv.csv") as csv_file:
+		reader = csv.reader("PRECIP_HLY_sample_csv.csv", delimiter = ',')
+
+	for row in reader ():
+
+		#first, make data into tensors
+		#Does row reference the individual data point?
+
+		dataobject = tf.Variable(row, tf.float64)
+		
+		#next, move each tensor to a dictionary
+		
+		datamover(dataholder, x, dataobject)
+		#for now, names of the feature are just ints but i know that's gross
+		
+		x = x+1
+
+		#add label
+
+		#move this to machinelearning2
